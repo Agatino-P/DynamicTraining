e37e5768063f0c0867ac9c8fa1bcba81fca1907f
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-04-05 23:48:18 +0200

    Add project files.

---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System.Windows;
+using WpfApp.Views;
+
+namespace WpfApp
+{
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new ShellWindow();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ WpfApp/ViewModels/ShellWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+
+namespace WpfApp.ViewModels
+{
+    public class ShellWindowViewModel : BindableBase
+    {
+
+        private string _text;
+        public string Text { get => _text; set => SetProperty(ref _text, value); }
+
+        public ShellWindowViewModel()
+        {
+            TestCmd = new(test);
+        }
+
+        public DelegateCommand TestCmd { get; private set; }
+        private void test()
+        {
+            dynamic dc = DateTime.Now;
+            Text = dc.ToString("ddd dd MMM yyyy HH:mm:ss.FFF");
+            DateTime dts = dc;
+            Text = dts.  .ToString("ddd dd MMM yyyy HH:mm:ss.FFF");
+        }
+    }
+}
